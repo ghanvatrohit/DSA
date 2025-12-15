f30bbf313f0af5328ba8ddc6ec009106970607b3
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-12-15 20:55:33 +0530

    Add program to check positive, negative, or zero

--- level_first/CheckPositiveNegativeZero.java
+++ level_first/CheckPositiveNegativeZero.java
@@ -1,5 +1,19 @@
 package level_first;
+import java.util.Scanner;
 
 public class CheckPositiveNegativeZero {
-    
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the nnumber :- ");
+        int num = sc.nextInt();
+
+        if(num>0){
+            System.out.println("The value is positive");
+        }else if (num==0){
+            System.out.println("The number is zero");
+        }else{
+            System.out.println("The value is negative number");
+        }
+        sc.close();
+    }
 }
