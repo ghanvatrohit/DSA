c14eb55b5ce4e01321bfa4c182680b003951916f
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-12-16 12:55:22 +0530

    Add program to check even or odd number

---
+++ level_first/CheckEvenOdd.java
@@ -0,0 +1,23 @@
+package level_first;
+
+import java.util.Scanner;
+
+public class CheckEvenOdd {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println("Please enter a non-negative number");
+        } else if (num % 2 == 0) {
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+
+        sc.close();
+    }
+}
