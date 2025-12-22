f738ec6ba2875fbad791e91266642c533db21d65
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-12-22 18:01:29 +0530

    Add program to check if a number is divisible by both 3 and 5

---
+++ Phase_1/CheckDivisibleBy3And5.java
@@ -0,0 +1,17 @@
+package Phase_1;
+import java.util.Scanner;
+public class CheckDivisibleBy3And5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number = ");
+        int num = sc.nextInt();
+
+        if(num%5==0 && num%3==0){
+            System.out.println("The given number is Divisible by 3 and 5");
+        }else{
+            System.out.println("The given number is not Divisible by 3 and 5");
+        }
+        sc.close();
+    }
+}
