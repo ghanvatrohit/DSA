bbcca398331a538b5c69bad54a32b02da7729d22
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-12-24 00:13:29 +0530

    Fix output text in vowel or consonant program

--- Phase_1/CheckVowelOrConsonant.java
+++ Phase_1/CheckVowelOrConsonant.java
@@ -7,7 +7,7 @@ public class CheckVowelOrConsonant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the character :-");
+        System.out.print("Enter the character :- ");
         char ch = sc.next().charAt(0);
 
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
