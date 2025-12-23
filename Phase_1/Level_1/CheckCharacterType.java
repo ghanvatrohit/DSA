90b1e05e59b7dfed0a53716f6c34844263e80fd7
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-12-24 00:35:28 +0530

    Add program to check character type (uppercase, lowercase, digit, special)

--- Phase_1/Level_1/CheckCharacterType.java
+++ Phase_1/Level_1/CheckCharacterType.java
@@ -1,5 +1,24 @@
 package Phase_1.Level_1;
 
+import java.util.Scanner;
+
 public class CheckCharacterType {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the character :- ");
+        char ch = sc.next().charAt(0);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lower case");
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("Upper case");
+        } else if (ch >= '0' && ch <= '9') {
+            System.out.println("Digit");
+        } else {
+            System.out.println("Special character");
+        }
+        sc.close();
+
+    }
 }
