d26fc2f5abde0eb8585e1fc22797ac09b4f62db1
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-12-29 00:00:15 +0530

    Add program to check if all digits of a 3-digit number are distinct

--- Phase_1/Level_3/CheckDistinctDigits.java
+++ Phase_1/Level_3/CheckDistinctDigits.java
@@ -9,18 +9,17 @@ public class CheckDistinctDigits {
         System.out.print("Enter a 3-digit number = ");
         int num = sc.nextInt();
 
-        // Check if number is 3-digit
         if (num < 100 || num > 999) {
             System.out.println("Invalid input (not a 3-digit number)");
         } else {
-            int d1 = num / 100;        // hundreds digit
-            int d2 = (num / 10) % 10; // tens digit
-            int d3 = num % 10;        // units digit
+            int d1 = num / 100;        // hundreds
+            int d2 = (num / 10) % 10; // tens
+            int d3 = num % 10;        // units
 
-            if (d1 != d2 && d2 != d3 && d1 != d3) {
-                System.out.println("All digits are distinct");
-            } else {
+            if (d1 == d2 || d2 == d3 || d1 == d3) {
                 System.out.println("Digits are not distinct");
+            } else {
+                System.out.println("All digits are distinct");
             }
         }
 
