971e0c94b989320842f3caeeccc0ecc1f272678a
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2025-12-28 22:07:27 +0530

    Add program to print day name based on day number

---
+++ Phase_1/Level_2/DayNameByNumber.java
@@ -0,0 +1,41 @@
+// 9. Take a day number (1–7) and print the corresponding day name.
+package Phase_1.Level_2;
+import java.util.Scanner;
+
+public class DayNameByNumber {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter day number (1–7) = ");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day number");
+        }
+
+        sc.close();
+    }
+}
