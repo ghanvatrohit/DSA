2df21e564894a7727f8f627eca03fb3ee71922e7
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-04 12:27:58 +0530

    Fix program to correctly print odd numbers between 1 and 100

--- Phase_2/PrintOddNumbers1To100.java
+++ Phase_2/PrintOddNumbers1To100.java
@@ -1,9 +1,12 @@
+// git commit -m "Add program to print even numbers between 1 and 100"
 package Phase_2;
 
 public class PrintOddNumbers1To100 {
     public static void main(String[] args) {
         for(int i=1 ; i<=100 ;i++ ){
-            
+            if(i%2!=0){
+                System.out.print(i+" ");
+            }
         }
     }
 }
