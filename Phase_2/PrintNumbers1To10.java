6e6a7235b933cd82cbcfbfc0629a3628f279ab42
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-04 12:12:08 +0530

    Add program to print numbers from 1 to 10 using for loop

---
+++ Phase_2/PrintNumbers1To10.java
@@ -0,0 +1,10 @@
+// 1. Print numbers from 1 to 10.
+package Phase_2;
+
+public class PrintNumbers1To10 {
+    public static void main(String[] args) {
+        for(int i=1 ; i<=10 ;i++){
+            System.out.println(i);
+        }
+    }
+}
