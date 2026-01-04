1968a10a90bbbc7d4a5fab81fcc8a529a537ca11
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-04 13:07:38 +0530

    Add program to print numbers from 10 down to 1 using for loop

---
+++ Phase_2/PrintNumbers10To1.java
@@ -0,0 +1,10 @@
+// 4. Print numbers from 10 down to 1.
+package Phase_2;
+
+public class PrintNumbers10To1 {
+    public static void main(String[] args) {
+        for(int i=10;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
