d1be3176ee9fadd4ca6dc4cd6db096af874e5046
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-04 12:17:48 +0530

    Add program to print even numbers between 1 and 100

---
+++ Phase_2/PrintEvenNumbers1To100.java
@@ -0,0 +1,11 @@
+package Phase_2;
+
+public class PrintEvenNumbers1To100 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i+", ");
+            }
+        }
+    }
+}
