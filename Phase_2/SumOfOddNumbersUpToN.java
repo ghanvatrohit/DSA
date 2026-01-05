1a45fa509a785ea731ebc129503885d5eba8adb3
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-05 23:29:13 +0530

    add program to calculate sum of odd numbers up to n using loop

---
+++ Phase_2/SumOfOddNumbersUpToN.java
@@ -0,0 +1,19 @@
+// 8. Print the sum of all odd numbers up to n.
+package Phase_2;
+
+import java.util.Scanner;
+
+public class SumOfOddNumbersUpToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n to print sum of all odd numbers = ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i += 2) {
+            sum += i;
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
