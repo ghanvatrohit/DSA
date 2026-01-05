a02c1594a9028f0bad09b84a5ab217272fe5fbde
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-05 23:04:09 +0530

    Add program to calculate sum of first n natural numbers using loop

---
+++ Phase_2/SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,22 @@
+// 6. Print the sum of first n natural numbers.
+package Phase_2;
+
+import java.util.Scanner;
+
+public class SumOfFirstNNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n to print sum of natural number =");
+        int n = sc.nextInt();
+        int sum = 0;
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                sum = sum + i;
+            }
+            System.out.print(+sum);
+        } else {
+            System.out.print("Invalid Input");
+        }
+        sc.close();
+    }
+}
