112da782fce9118e4a7cb180da6f6d2eeec2a1a0
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-05 23:17:12 +0530

    add program to calculate sum of even numbers up to n using loop

---
+++ Phase_2/SumOfEvenNumbersUpToN.java
@@ -0,0 +1,19 @@
+//7. Print the sum of all even numbers up to n.
+package Phase_2;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumbersUpToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n to print sum of even number = ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i <= n; i=i+2) {//i=i+2 or i+=2
+            sum = sum + i;
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
