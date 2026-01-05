cb317f3a5d7ffc7f1dcefbee82ea189f7482c269
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-05 23:55:04 +0530

    add program to calculate product of digits of a given number

---
+++ Phase_2/ProductOfDigitsOfNumber.java
@@ -0,0 +1,23 @@
+package Phase_2;
+
+import java.util.Scanner;
+
+public class ProductOfDigitsOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int product = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product = product * digit;
+            n = n / 10;
+        }
+
+        System.out.print(product);
+        sc.close();
+    }
+}
