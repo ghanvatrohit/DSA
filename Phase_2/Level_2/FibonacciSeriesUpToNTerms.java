9a060bc7da4603db33b4c9a312990f081dce22d2
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-07 15:47:09 +0530

    add program to print Fibonacci series up to n terms

---
+++ Phase_2/Level_2/FibonacciSeriesUpToNTerms.java
@@ -0,0 +1,26 @@
+// 9. Print Fibonacci series up to n terms.
+package Phase_2.Level_2;
+import java.util.Scanner;
+
+public class FibonacciSeriesUpToNTerms {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to print Fibonacci Series Up To 'N' Terms = ");
+        int n = sc.nextInt();
+        int a=0;
+        int b=1;
+
+        if(n >= 1){
+            System.out.print(a + " ");
+        }
+        if(n>=2){
+            System.out.print(b +" ");
+        }
+        for(int i=3 ; i<=n ; i++){
+            int c = a + b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }sc.close();
+    }
+}
