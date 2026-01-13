2d93040dc8165050b6a5d01b80cbfdb34178ab4e
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-13 23:28:23 +0530

    print n stars on the same line using loop

--- Practice/PrintNStarsOnSameLine.java
+++ Practice/PrintNStarsOnSameLine.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class PrintNStarsOnSameLine {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to print = ");
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n ;i++){
+            System.out.print("*");
+        }
+        sc.close();
     }
 }
