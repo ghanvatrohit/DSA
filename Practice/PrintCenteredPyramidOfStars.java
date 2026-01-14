cd2c5f94170a904d8340f3515beffe80b252fd55
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-14 20:16:51 +0530

    correct space calculation for centered pyramid pattern

---
+++ Practice/PrintCenteredPyramidOfStars.java
@@ -0,0 +1,20 @@
+// 9. Print a Centered Pyramid of Stars
+import java.util.Scanner;
+
+public class PrintCenteredPyramidOfStars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number = ");
+        int n = sc.nextInt();
+
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n-i ; j++){
+                System.out.print(" ");
+            }
+            for(int k=0 ;k<2*i-1 ;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }sc.close();
+    }
+}
