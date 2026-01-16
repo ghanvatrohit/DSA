a92ed6bcff61f253d2085fadceea02c4798607a9
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-16 13:46:33 +0530

    print numbers from 1 to 100 whose digit sum is even

--- Phase_2/Level_4/PrintNumbersWithEvenDigitSum.java
+++ Phase_2/Level_4/PrintNumbersWithEvenDigitSum.java
@@ -2,14 +2,14 @@ package Phase_2.Level_4;
 
 public class PrintNumbersWithEvenDigitSum {
     public static void main(String[] args) {
-    int sum = 0;
-    for(int i=1; i<100; i++){
-        int d1 = i%10;
-        int d2 = i/10;
-        sum = d1+d2;
-        if(sum%2==0){
-            System.out.println(i);
+        int sum = 0;
+        for (int i = 1; i < 100; i++) {
+            int d1 = i % 10;
+            int d2 = i / 10;
+            sum = d1 + d2;
+            if (sum % 2 == 0) {
+                System.out.println(i);
+            }
         }
-    }
     }
 }
