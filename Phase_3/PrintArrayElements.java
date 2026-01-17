c2e9068dd173419f4d3e6b92100ed09e29921343
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-17 22:18:31 +0530

    Add program to input and print array elements

---
+++ Phase_3/PrintArrayElements.java
@@ -0,0 +1,23 @@
+// 1. Input n and take n integers into an array; print them
+package Phase_3;
+import java.util.Scanner;
+
+public class PrintArrayElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements = ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter "+n+" elements = ");
+        for(int i=0 ; i<n ;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0 ; i<n ; i++){
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
