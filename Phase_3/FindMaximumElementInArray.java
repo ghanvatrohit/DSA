b8db5b4dc2abe0e369a18c71ea8ec2a1f6dcf4fd
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-18 20:23:58 +0530

    Fix and finalize logic to find maximum element in an array

---
+++ Phase_3/FindMaximumElementInArray.java
@@ -0,0 +1,28 @@
+package Phase_3;
+
+import java.util.Scanner;
+
+public class FindMaximumElementInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array = ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " value.");
+        for (int i = 0; i <arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int large = arr[0];
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (large<arr[i]) {
+                large = arr[i];
+            }
+        }
+        System.out.println("Large number is = "+ large);
+        sc.close();
+    }
+}
