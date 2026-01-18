7e112be8c804a4b46a977b4139ba485fbe7f9abb
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-18 23:08:23 +0530

    Add program to find minimum element in an array

---
+++ Phase_3/FindMinimumElementInArray.java
@@ -0,0 +1,26 @@
+package Phase_3;
+import java.util.Scanner;
+
+public class FindMinimumElementInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array = ");
+        int n = sc.nextInt();
+
+        int[] arr= new int[n];
+
+        System.out.println("Enter "+n+" value");
+        for(int i=0 ; i<arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+        for(int i=0 ; i<arr.length ; i++){
+            if(min>arr[i]){
+                min = arr[i];
+            }
+        }
+        System.out.println("The smallest number is "+min);
+        sc.close();
+    }
+}
