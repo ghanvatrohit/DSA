b0ef0ab96b15dd59357f90a6fc99dd350d80bee1
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-24 12:18:30 +0530

    Add program to print array elements greater than a given value k

---
+++ Phase_3/PrintElementsGreaterThanK.java
@@ -0,0 +1,26 @@
+package Phase_3;
+import java.util.Scanner;
+
+public class PrintElementsGreaterThanK {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array = ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter the "+n+" value = ");
+        for(int i=0 ; i<arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("\nEnter the value of 'K' = ");
+        int k = sc.nextInt();
+        for(int i=0 ; i<arr.length ; i++){
+            if(k<arr[i]){
+                System.out.println(arr[i]+" is greater than "+k);
+            }
+        }
+        sc.close();
+    }
+}
