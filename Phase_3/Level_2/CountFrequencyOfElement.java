f38606fead71e32fbfceeb9c4f410526bee7d035
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-01-25 10:19:22 +0530

    Add program to count frequency of a given element in an array

---
+++ Phase_3/Level_2/CountFrequencyOfElement.java
@@ -0,0 +1,28 @@
+package Phase_3.Level_2;
+import java.util.Scanner;
+
+public class CountFrequencyOfElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array = ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("\nEnter the "+n+" value = ");
+        for(int i=0 ; i<arr.length ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("\nWhich element to check how many times appear = ");
+        int x = sc.nextInt();
+        int count = 0;
+        for(int i=0 ; i<arr.length ; i++){
+            if(arr[i]==x){
+                count++;
+            }
+        }
+        System.out.print("\nThe given element appears " + count + " times");
+        sc.close();
+    }
+}
