e3e8650da52f23712b666b7ca01cbce44c17712c
ROHIT NAMDEO GHANVAT <rohitghanvat2004@gmail.com>
2026-02-07 22:30:11 +0530

    Attempted array reversal but used incorrect loop condition preventing swaps

---
+++ Phase_3/Level_3/ReverseArray.java
@@ -0,0 +1,36 @@
+// 6. Reverse an array (without using built-in reverse).
+package Phase_3.Level_3;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array = ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
